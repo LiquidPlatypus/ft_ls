5154b94a16c5818c54ab5dfd5c9c901e97868865
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2026-09-23 14:18:06 +0200

    created utils file

---
+++ utils.c
@@ -0,0 +1,42 @@
+#include "ft_ls.h"
+
+
+t_args parse_args(int ac, char **av)
+{
+	t_args	args;
+	int		i;
+	int		path_count;
+
+	args.flags = 0;
+	args.paths = malloc(sizeof(char *) * (ac + 1));
+	path_count = 0;
+	i = 1;
+	while (i < ac)
+	{
+		if (av[i][0] == '-')
+		{
+			if (ft_strchr(av[i], 'l'))
+				args.flags |= l;
+			if (ft_strchr(av[i], 'R'))
+				args.flags |= R;
+			if (ft_strchr(av[i], 'r'))
+				args.flags |= r;
+			if (ft_strchr(av[i], 'a'))
+				args.flags |= a;
+			if (ft_strchr(av[i], 't'))
+				args.flags |= t;
+		}
+		else
+			args.paths[path_count++] = av[i];
+		i++;
+	}
+	if (path_count == 0)
+		args.paths[path_count++] = ".";
+	args.paths[path_count] = NULL;
+	return (args);
+}
+
+
+int compare(const void *a, const void *b) {
+	return ft_strncmp(*(char **)a, *(char **)b, SIZE_MAX);
+}
